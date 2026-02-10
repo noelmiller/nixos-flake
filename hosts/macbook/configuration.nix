e8fcc2b6ea414dcea658cd80678caa803d9ac9d8
Noel Miller <noel@noelmiller.dev>
2026-02-09 19:20:01 -0600

    re-enable X11

--- hosts/macbook/configuration.nix
+++ hosts/macbook/configuration.nix
@@ -36,7 +36,7 @@
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = false;
+  services.xserver.enable = true;
 
   # enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
