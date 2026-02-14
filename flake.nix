154ecaad7f24eb7c555405fb5d0cb5ccc3f8b495
Noel Miller <noel@noelmiller.dev>
2026-02-14 14:48:23 -0600

    update flake

--- flake.nix
+++ flake.nix
@@ -2,13 +2,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    scopebuddy.url = "github:OpenGamingCollective/ScopeBuddy";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, scopebuddy, ... }:
     let
       # Create an overlay to make unstable packages available
       overlay-unstable = final: prev: {
@@ -36,6 +37,7 @@
 
         desktop = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
+          specialArgs = { inherit scopebuddy; };
           modules = [
             ./hosts/desktop/configuration.nix
             home-manager.nixosModules.home-manager
