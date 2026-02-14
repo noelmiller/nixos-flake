2bdcf69c6d7c0d108ab2ce88232e70fa4f5231e2
Noel Miller <noel@noelmiller.dev>
2026-02-14 14:48:02 -0600

    add data drives and gaming.nix

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -8,12 +8,24 @@
       ../../modules/containers.nix
       ../../modules/flatpak.nix
       ../../modules/kde.nix
+      ../../modules/gaming.nix
       ../../modules/packages.nix
       ../../modules/programming.nix
       ../../modules/virtualisation.nix
     ];
 
   networking.hostName = "desktop"; # Define your hostname.
+
+  # Additional data drives
+  fileSystems."/mnt/nvme_2t" = {
+    device = "/dev/disk/by-uuid/29482386-070a-495e-b66c-8f0be0a994fe";
+    fsType = "btrfs";
+  };
+
+  fileSystems."/mnt/ssd_8t" = {
+    device = "/dev/disk/by-uuid/3d4fecf4-5870-4132-a81d-b5feb4aa7371";
+    fsType = "btrfs";
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
