a81c7331017ba41a3f3798471b99438c8b12bb2c
Noel Miller <noel@noelmiller.dev>
2026-02-14 11:15:47 -0600

    add firefox configuration

--- home.nix
+++ home.nix
@@ -3,13 +3,13 @@
 {
   home.username = "noel";
   home.homeDirectory = "/home/noel";
-  home.stateVersion = "25.11"; # Matches your flake input version
+  home.stateVersion = "25.11";
 
-  # User-specific packages
+  # user-specific packages
   home.packages = with pkgs; [
   ];
 
-  # Fish shell configuration
+  # fish shell configuration
   programs.fish = {
     enable = true;
     shellAbbrs = {
@@ -20,7 +20,7 @@
     };
   };
 
-  # Git configuration
+  # git configuration
   programs.git = {
     enable = true;
     settings = {
@@ -29,6 +29,63 @@
         email = "noel@noelmiller.dev";
         init.defaultBranch = "main";
       };
+    };
+  };
+
+  # firefox configuration
+  programs.firefox = {
+    enable = true;
+    policies = {
+      AutofillAddressEnabled = false;
+      AutofillCreditCardEnabled = false;
+      DisableFirefoxStudies = true;
+      DisableFormHistory = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DisplayBookmarksToolbar = "always";
+      FirefoxHome = {
+        Search = true;
+        TopSites = false;
+        SponsoredTopSites = false;
+        Highlights = false;
+        Pocket = false;
+        Stories = false;
+        SponsoredPocket = false;
+        SponsoredStories = false;
+        Snippets = false;
+      };
+      GenerativeAI = false;
+      HardwareAcceleration = true;
+      OfferToSaveLogins = false;
+      OverrideFirstRunPage = "";
+      PasswordManagerEnabled = false;
+      SearchEngines = {
+        Default = "Brave Search";
+        Add = [
+          {
+            Name = "Brave Search";
+            URLTemplate = "https://search.brave.com/search?q={searchTerms}&amp";
+            IconURL = "https://cdn.search.brave.com/serp/v1/static/brand/eebf5f2ce06b0b0ee6bbd72d7e18621d4618b9663471d42463c692d019068072-brave-lion-favicon.png";
+            Alias = "brave";
+          }
+        ];
+      };
+      ExtensionSettings = builtins.listToAttrs (
+      builtins.map (id: {
+        name = id;
+        value = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/${id}/latest.xpi";
+          installation_mode = "force_installed";
+          default_area = "navbar";
+        };
+      }) [
+        "uBlock0@raymondhill.net"                # uBlock Origin
+        "{d7742d87-e61d-4b78-b8a1-b469842139fa}" # Vimium
+        "addon@darkreader.org"                   # Dark Reader
+        "{d634138d-c276-4fc8-924b-40a0ea21d284}" # 1Password
+        "{019b606a-6f61-4d01-af2a-cea528f606da}" # XBrowserSync
+      ]
+    );
     };
   };
 
