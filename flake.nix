0ff45bb28fa3269d0c1efc2dc32c895e8e6a6989
Noel Miller <noel@noelmiller.dev>
2026-02-14 11:22:27 -0600

    update flake to support unstable

--- flake.nix
+++ flake.nix
@@ -1,41 +1,52 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
-    nixosConfigurations = {
-      # This matches your 'macbook' directory
-      macbook = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ 
-          ./hosts/macbook/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.noel = import ./home.nix;
-          }
-        ];
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
+    let
+      # Create an overlay to make unstable packages available
+      overlay-unstable = final: prev: {
+        unstable = import nixpkgs-unstable {
+          system = prev.system;
+          config.allowUnfree = true;
+        };
       };
+    in
+    {
+      nixosConfigurations = {
+        macbook = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/macbook/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              nixpkgs.overlays = [ overlay-unstable ];
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.noel = import ./home.nix;
+            }
+          ];
+        };
 
-      # This matches your 'desktop' directory
-      desktop = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ 
-          ./hosts/desktop/configuration.nix
-             home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.noel = import ./home.nix;
-          }
-        ];
+        desktop = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/desktop/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              nixpkgs.overlays = [ overlay-unstable ];
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.noel = import ./home.nix;
+            }
+          ];
+        };
       };
     };
-  };
 }
