9c6f54d1c38b3ac643366746a3826c25fba26770
Noel Miller <noel@noelmiller.dev>
2026-02-21 23:41:41 -0600

    add function for creating rust development environment

--- home.nix
+++ home.nix
@@ -30,13 +30,29 @@
     interactiveShellInit = ''
       carapace _carapace | source
     '';
+
+    functions = {
+      new-rust = {
+        body = ''
+          if test -n "$argv[1]"
+            git clone --depth 1 https://github.com/noelmiller/rust-template.git $argv[1]
+            and cd $argv[1]
+            and rm -rf .git
+            and direnv allow
+            echo "Successfully initialized $argv[1] from template."
+          else
+            echo "Error: Please provide a project name."
+            return 1
+          end
+        '';
+      };
+    };
+    
     shellAbbrs = {
       deploy = "sudo nixos-rebuild switch --flake /home/noel/repos/nixos#${osConfig.networking.hostName}";
       update = "nix flake update --flake /home/noel/repos/nixos";
       full-upgrade = "nix flake update --flake /home/noel/repos/nixos && sudo nixos-rebuild switch --flake /home/noel/repos/nixos#${osConfig.networking.hostName}";
       clean = "sudo nix-collect-garbage -d";
-      rust-init = "nix flake init --template \"https://flakehub.com/f/the-nix-way/dev-templates/*#rust\"";
-      rust-project = "nix flake new --template \"https://flakehub.com/f/the-nix-way/dev-templates/*#rust\"";
     };
   };
 
