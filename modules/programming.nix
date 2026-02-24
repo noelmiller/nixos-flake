b5b3706869d09e03c7d2f0c1f913020edda6f8af
Noel Miller <noel@noelmiller.dev>
2026-02-23 20:57:41 -0600

    remove vscode and emacs

--- modules/programming.nix
+++ modules/programming.nix
@@ -16,15 +16,7 @@
     minikube
     nil # used for nix language server
     nixd # used for nix language server
-    vscode
     wget
     zed-editor
   ];
-
-  # install and configure emacs
-  services.emacs = {
-    enable = true;
-    package = (pkgs.emacsPackagesFor pkgs.emacs-pgtk).emacsWithPackages (epkgs: [ epkgs.vterm ]);
-    defaultEditor = true;
-  };
 }
